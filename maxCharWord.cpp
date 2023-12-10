566c48e12e8c3129227c4eb7ee22150b21a08982
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-12-10 17:09:32 +0300

    Add files via upload

--- maxCharWord.cpp
+++ maxCharWord.cpp
@@ -1,29 +1,44 @@
 #include "middle_str.h"
 
-std::string max(std::string str1, std::string str2)
+bool IsSpace(char i)
 {
-	if (str2 > str1)
-		return str2;
-	return str1;
+    if (i == ' ')
+        return true;
+    return false;
 }
 
 std::string itc_maxCharWord(std::string str)
 {
-	std::string result = " ";
-	std::string maxWord;
-	std::string currentWord;
-	bool inWord = false;
-	long long lenOfCurrenWord = 0;
-	for(int i = 0; i <= len(str); i++)
-	{
-		if (IsLetter(str[i]))
-		{
-			inWord = true;
-			lenOfCurrenWord++;
-		}
-		if(inWord && IsSpaceOrEndl(str[i]))
-		{
-			maxWord = max(maxWord, currentWord)
-		}
-	}
+    setlocale(LC_ALL, "Russian");
+    std::string result = "";
+    std::string currentWord;
+    long long currentLen = 0;
+    bool inWord = false;
+    bool inListOfLetters = false;
+    for (int i = 0; i <= len(str); i++)
+    {
+        if (IsLetter(str[i]))
+        {
+            inWord = true;
+            currentLen++;
+            currentWord += str[i];
+        }
+        if (inWord && IsSpaceOrEndl(str[i]))
+        {
+            if(len(currentWord) > len(result))
+                result = currentWord;
+            currentWord = "";
+            currentLen = 0;
+            inWord = false;
+        }
+        if (inWord && !IsLetter(str[i]) && !IsSpaceOrEndl(str[i]))
+        {
+            while (str[i] != ' ' && i < len(str))
+                i++;
+            inWord = false;
+        }
+    }
+    if (result == "")
+        return "error";
+    return result;
 }
