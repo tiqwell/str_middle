6a818bb1685d38ba168341fabab0c553e0c85a1c
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-12-11 10:45:34 +0300

    Add files via upload

---
+++ isFirstInSecond.cpp
@@ -0,0 +1,21 @@
+#include "str_middle.h"
+
+bool itc_isFirstInSecond(std::string s1, std::string s2)
+{
+	int a = 0;
+	int result = 1;
+	for(int i = 0; i < len(s2); i++)
+	{
+		if(s2[i] == s1[0])
+		{
+			for(int j = i; j < len(s1) + i; j++)
+			{
+				if (s2[j] == s1[j - i])
+					result++;
+			}
+			if (result == len(s1))
+				return true;
+		}
+	}
+	return false;
+}
