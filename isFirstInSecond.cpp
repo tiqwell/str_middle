cc4d9eb6716b549ed3dbadaf631a92a35d516d95
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-12-18 10:00:07 +0300

    Update isFirstInSecond.cpp

--- isFirstInSecond.cpp
+++ isFirstInSecond.cpp
@@ -2,20 +2,16 @@
 
 bool itc_isFirstInSecond(std::string s1, std::string s2)
 {
-	int a = 0;
-	int result = 1;
-	for(int i = 0; i < len(s2); i++)
-	{
-		if(s2[i] == s1[0])
-		{
-			for(int j = i; j < len(s1) + i; j++)
-			{
-				if (s2[j] == s1[j - i])
-					result++;
-			}
-			if (result == len(s1))
-				return true;
-		}
-	}
-	return false;
+    for (long long i = 0; i <= len(s2) - len(s1); i++)
+    {
+        int result = 0;
+        for (long long j = 0; j < len(s1); j++)
+        {
+            if (s2[i + j] == s1[j])
+                result++;
+        }
+        if (result == len(s1))
+            return true;
+    }
+    return false;
 }
