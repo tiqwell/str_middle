17e5e741884bad254f3cd1e751a9b3a71174d48c
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-12-18 11:01:15 +0300

    Update Cezar.cpp

--- Cezar.cpp
+++ Cezar.cpp
@@ -2,13 +2,22 @@
 
 std::string itc_Cezar(std::string str, int k) 
 {
-	for(int i = 0; i < len(str); i++)
-	{
-		str[i] += k;
-		if (str[i] > 'z')
-			str[i] -= 'z' - 'a' + 1;
-		if (str[i] < 'a')
-			str[i] += 'z' - 'a' + 1;
-	}
-	return str;
+    for (int i = 0; i < len(str); i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = (str[i] - 'a' + k) % 26 + 'a';
+
+            if (str[i] < 'a')
+                str[i] += 26;
+        }
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = (str[i] - 'A' + k) % 26 + 'A';
+
+            if (str[i] < 'A')
+                str[i] += 26;
+        }
+    }
+    return str;
 }
