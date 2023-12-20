4f0fedc2d01230ca2417a3b2ab7d25976d263381
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-12-20 12:08:43 +0300

    Update rmFreeSpace.cpp

--- rmFreeSpace.cpp
+++ rmFreeSpace.cpp
@@ -3,16 +3,20 @@
 std::string itc_rmFreeSpace(std::string str)
 {
 	std::string result;
-	for (int i = 0; i < len(str); i++)
+	int a = 0;
+	for(int i = 0; str[i] == ' '; i++)
+		a++;
+	for (int i = a; i < len(str); i++)
 	{
 		if (str[i] != ' ')
 			result += str[i];
 		else
 		{
-			result += ' ';
-			while (str[i + 1] == ' ')
+			while (str[i + 1] == ' ') 
 				i++;
+			result += ' ';
 		}
 	}
+	result.pop_back();
 	return result;
 }
